31af1be1b62c6ee23e186651bae7f871131ed0e4
seanjessen <info@seanjessen.co.uk>
2021-02-22 17:06:48 +0000

    Added header layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { ThemeProvider } from '@material-ui/core/styles';
 
+// Import app styling
 import theme from './theme';
 import './App.css';
+
+// Import components
+import Header from './components/Header';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
+        <Header />
       </div>
     </ThemeProvider>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
+
+import Logo from './../assets/nhs-leeds-and-yorkshire-partnership.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <Box
+        component="header"
+        px={6}
+        py={4}>
+        <Grid
+          container
+          spacing={3}
+          justify="space-between"
+          alignItems="center">
+          <Grid item>
+            Wifi online
+          </Grid>
+
+          <Grid item>
+            <img src={Logo} alt="NHS: Leeds and York Partnership - NHS Foundation Trust" />
+            <Typography variant="h4" align="right">
+              Ward Name
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    );
+  }
+}
+
+export default Header;
