1b2761c4756efb3671b4917e69a86e1b284b5754
seanjessen <info@seanjessen.co.uk>
2021-02-25 17:26:47 +0000

    Added offline indicator

--- src/components/Network-status.js
+++ src/components/Network-status.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import { Detector } from 'react-detect-offline';
 
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 
 import { ReactComponent as OnlineIndicator } from './../assets/online-indicator.svg';
+import { ReactComponent as OfflineIndicator } from './../assets/offline-indicator.svg';
 
 class NetworkStatus extends Component {
   render() {
     return (
       <Box
+        maxWidth={396}
         px={3}
-        py={3}
+        py={2}
         border={1}
         borderColor="common.gray"
         borderRadius={14}
@@ -20,19 +23,34 @@ class NetworkStatus extends Component {
         variant="outlined">
         <Grid
           container
+          style={{ height: "111px" }}
           spacing={2}
           justify="space-between"
           alignItems="center">
           <Grid item>
-            <OnlineIndicator />
+            <Detector
+              render={({ online }) => (online ? <OnlineIndicator /> : <OfflineIndicator />)}
+            />
           </Grid>
 
-          <Grid item>
+          <Grid item xs>
             <Typography variant="h4">
-              Wifi Online
+              <Detector
+                render={({ online }) => (
+                  <div>
+                    Wifi {online ? "Online" : "Offline"}
+                  </div>
+                )}
+              />
             </Typography>
             <Typography variant="body2" color="textSecondary">
-              You can make a submission
+              <Detector
+                render={({ online }) => (
+                  <div>
+                    You can make a submission {!online && "but can't submit until you're connected"}
+                  </div>
+                )}
+              />
             </Typography>
           </Grid>
         </Grid>
